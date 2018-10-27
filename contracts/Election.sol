6be787319705003034df55f7eef59ec6bd28744a
VitalPointAI <a.luhning@vitalpoint.ai>
2018-10-27 12:54:29 -0400

    setup of front end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,26 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    //Read/write candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    //Store Candidates count
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }

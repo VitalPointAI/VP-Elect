c1035d4c8fcda497b7b1ab4218c25848ce291606
VitalPointAI <a.luhning@vitalpoint.ai>
2018-10-27 16:53:47 -0400

    Drizzle integration

--- front/src/index.js
+++ front/src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Drizzle, generateStore } from 'drizzle';
+
+//import contracts
+import Elections from '../../build/contracts/Election.json';
+
+const options = {
+    web3: {
+        block: false,
+        fallback: {
+            type: "ws",
+            url: "ws://127.0.0.1:7545"
+        }
+    },
+    contracts: [Elections],
+    events: {}
+};
+
+//Setup Drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+ReactDOM.render(<App drizzle={drizzle} />, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
